fdaabf39f57a22d138b9cd8228f10aa8a40ed2bc
devansh-sudo <devanshdixit97@gmail.com>
2023-08-13 12:45:28 +0530

    updates

--- server.js
+++ server.js
@@ -3,11 +3,18 @@ const connectDb = require("./config/dbConnection");
 const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
 const cors = require('cors');
+const https = require('https');
+const fs = require('fs');
 
 connectDb();
 const app = express();
 
 const port = process.env.PORT || 3001;
+
+const options = {
+    cert: fs.readFileSync('/etc/letsencrypt/live/supremebackoffice.in/fullchain.pem'),
+    key: fs.readFileSync('/etc/letsencrypt/live/supremebackoffice.in/privkey.pem')
+};
 
 app.use(cors())
 app.use(express.json());
@@ -18,6 +25,7 @@ app.use("/api/v1/appointment", require("./routes/appointmentRoutes"))
 app.use("/api/v1/tests", require("./routes/testsRoutes"))
 app.use(errorHandler);
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+const server = https.createServer(options, app); // Assuming 'app' is your Express app
+server.listen(port, () => {
+    console.log(`Server is running on port ${port} over HTTPS`);
 });
